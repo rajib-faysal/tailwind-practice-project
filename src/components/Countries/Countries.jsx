9f1bbcd7070dcf77e9f5e7305ad8ee476a9f0b94
rajib-faysal <rajib.faysal.201@gmail.com>
2024-08-28 02:34:42 +0600

    added filtering button and fuctionality

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,26 +1,67 @@
 import { useEffect, useState } from 'react';
+import { v4 as uuid } from 'uuid';
 import Country from '../Country/Country';
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [reload, setReload] = useState(false);
+  const [freeBtn, setFreeBtn] = useState(false);
+  const [notFreeBtn, setNotFreeBtn] = useState(false);
 
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
       .then((res) => res.json())
-      .then((data) => setCountries(data.slice(0, 10)));
-  }, []);
+      .then((data) => setCountries(data));
+  }, [reload]);
+
+  const handleFree = () => {
+    setNotFreeBtn(!notFreeBtn);
+    const filteredCountry = countries.filter((c) => c.independent);
+    setCountries(() => filteredCountry);
+  };
+  const handleNotFree = () => {
+    setNotFreeBtn(!freeBtn);
+    const filteredCountry = countries.filter((c) => !c.independent);
+    setCountries(() => filteredCountry);
+  };
+
+  const handleReload = () => {
+    setNotFreeBtn(false);
+    setFreeBtn(false);
+    setReload(() => !reload);
+  };
 
   return (
-    <div className="flex flex-wrap">
-      {countries.map((c) => (
-        // <h1
-        //   className="block text-bold text-4xl text-orange-400 m-4 font-extrabold"
-        //   key={c.name.common}
-        // >
-        //   {c.name.common}
-        // </h1>
-        <Country key={c.name.common} country={c} />
-      ))}
+    <div className="m-10">
+      <button
+        disabled={notFreeBtn}
+        className="btn btn-primary text-3xl ml-3"
+        onClick={handleFree}
+      >
+        Free
+      </button>
+      <button
+        disabled={freeBtn}
+        className="btn btn-primary text-3xl ml-3"
+        onClick={handleNotFree}
+      >
+        Not Free
+      </button>
+      <button className="btn btn-primary text-3xl ml-3" onClick={handleReload}>
+        Reload
+      </button>
+      <div className="flex flex-wrap">
+        <button></button>
+        {countries.map((c) => (
+          // <h1
+          //   className="block text-bold text-4xl text-orange-400 m-4 font-extrabold"
+          //   key={c.name.common}
+          // >
+          //   {c.name.common}
+          // </h1>
+          <Country key={uuid()} country={c} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,15 @@
 const Country = ({ country }) => {
+  const { name, independent, flags, population, area } = country;
   return (
-    <div className="border border-orange-400 m-5 p-5 rounded-md text-center w-96 h-48 flex flex-col items-center justify-center gap-3">
-      <h1 className="text-bold text-3xl text-orange-400">
-        {country.name?.common}
-      </h1>
+    <div className="border border-orange-400 m-5 p-5 rounded-md text-center w-96  flex flex-col items-center justify-center gap-3">
+      <h1 className="text-bold text-3xl text-orange-400">{name?.common}</h1>
       <p className="text-xl font-bold">
-        {country.independent ? 'Free! 😊' : 'We are not free! 😭'}
+        {independent ? 'Free! 😊' : 'We are not free! 😭'}
+      </p>
+      <img className="w-96 h-full" src={flags?.png} alt="" />
+      <p className="text-2xl font-medium ">Population: {population}</p>
+      <p className="text-2xl font-medium ">
+        Area: {area} Kilometers<sup>2</sup>
       </p>
     </div>
   );
